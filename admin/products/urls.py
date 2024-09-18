d20d6525d5ac1bcaccc14deb11e60f9fb7940f55
1mashilo <christophermashilo@gmail.com>
2024-09-18 14:29:36 +0200

    "Configured URL patterns for ProductViewSet and UserAPIView

---
+++ admin/products/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import ProductViewSet, UserAPIView, index
+
+urlpatterns = [
+    path('products', ProductViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+    }), name='product-list'),
+
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'delete',
+    }), name='product-detail'),
+
+    path('user', UserAPIView.as_view(), name='user-list'),
+    path('index', index, name='index'),
+]
