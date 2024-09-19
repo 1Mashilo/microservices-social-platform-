a6614a42731cd12d6c93e4e86cd36f34aaff97a6
1Mashilo <christophermashilo@gmail.com>
2024-09-19 19:21:03 +0200

    Added documentation to the RabbitMQ producer module.

---
+++ main/producer.py
@@ -0,0 +1,26 @@
+"""
+This module provides a function to publish messages to a RabbitMQ queue.
+
+It establishes a connection to a RabbitMQ server and defines a 'publish' 
+function to send messages with specified metadata (method) and content (body) 
+to the 'admin' queue.
+"""
+
+import pika
+import json
+
+params = pika.URLParameters('amqp://guest:guest@localhost:5672/')
+
+connection = pika.BlockingConnection(params)  
+channel = connection.channel() 
+
+def publish(method, body):
+    """
+    Publishes a message to the 'admin' queue.
+
+    Args:
+        method (str): The type of message or action being performed (e.g., 'product_created', 'product_updated').
+        body (dict): The data associated with the message, typically a dictionary that will be JSON-encoded.
+    """
+    properties = pika.BasicProperties(method) 
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
