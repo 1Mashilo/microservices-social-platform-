729e52478fee16d4a1d400fd231ada722e818b41
1Mashilo <christophermashilo@gmail.com>
2024-09-19 16:50:20 +0200

    Added docstring to ProductsConfig

---
+++ admin/products/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    """
+    Configuration class for the 'products' Django application.
+
+    Attributes:
+        name (str): The name of the application ('products').
+    """
+    name = 'products'
