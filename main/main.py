2e1bd8da23f687d7a16801d0c229b9454c16d8f4
1Mashilo <christophermashilo@gmail.com>
2024-09-19 12:58:48 +0200

    Implemented routes for listing products and liking a product, including RabbitMQ message publishing.

--- main/main.py
+++ main/main.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, UniqueConstraint
+from sqlalchemy import UniqueConstraint
+from producer import publish_to_main 
 from dataclasses import dataclass
 from urllib.parse import quote_plus
+import requests
 
 app = Flask(__name__)
 
@@ -13,16 +15,14 @@ load_dotenv()
 
 quoted_password = quote_plus(os.getenv("MYSQL_PASSWORD"))
 SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{quoted_password}@" 
+    f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{quoted_password}@"
     f"{os.getenv('MYSQL_DB_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DATABASE')}"
 )
 
 CORS(app)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db = SQLAlchemy(app)
 
@@ -35,8 +35,7 @@ class Product(db.Model):
     title: str
     image: str
 
-    id = db.Column(db.Integer, primary_key=True, 
-                   autoincrement=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
@@ -44,13 +43,36 @@ class Product(db.Model):
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer) 
-
+    product_id = db.Column(db.Integer)
 
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 # ------------------------------------------------------------------------
+# Routes
 
+@app.route('/api/products')
+def index():
+    return jsonify(Product.query.all())
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://docker.for.win.localhost:8000/api/user')
+    json = req.json()
+
+    try:
+        productUser = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+
+        publish('product_liked', id)
+    except:
+        abort(400, 'You already liked this product')
+
+    return jsonify({
+        'message': 'success',
+        'id': json['id']
+    })
+
+# ------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True, 
-            host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
