cb7aa9b06ac1464dd71df670db1ac16e9c106c64
1Mashilo <christophermashilo@gmail.com>
2024-09-24 21:39:12 +0200

    - Added RegistrationSerializer for user registration with password validation. - Created LoginSerializer for user authentication. - Developed UserSerializer for user details serialization.

--- admin/products/serializers.py
+++ admin/products/serializers.py
@@ -1,21 +1,46 @@
 from rest_framework import serializers
 from .models import Product
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        """
-        Meta class for ProductSerializer.
-
-        Attributes:
-            model (class): The model class to serialize.
-            fields (list): The fields to include in the serialized output.
-        """
         model = Product
         fields = '__all__'
 
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={"input_type": "password"})
+
+    class Meta:
+        model = get_user_model()
+        fields = ("first_name", "last_name", "email", "password", "password2")
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "password2": {"write_only": True}
+        }
+
+    def save(self):
+        user = get_user_model()(
+            email=self.validated_data["email"],
+            first_name=self.validated_data["first_name"],
+            last_name=self.validated_data["last_name"],
+        )
+
+        password = self.validated_data["password"]
+        password2 = self.validated_data["password2"]
+
+        if password != password2:
+            raise serializers.ValidationError({"password": "Passwords do not match!"})
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password'] 
-        extra_kwargs = {'password': {'write_only': True}}  
+        model = get_user_model()
+        fields = ("id", "email", "is_staff", "first_name", "last_name")
